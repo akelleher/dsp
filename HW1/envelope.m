1f0f0c8e4623fa7da8d8c8e41796ed2800118379
Alex Kelleher <alexpkelleher@gmail.com>
2016-09-11 23:20:08 -0400

    correct time values

--- HW1/envelope.m
+++ HW1/envelope.m
@@ -3,22 +3,22 @@ function [t,e] = envelope(fs,a,d,s,dur,r)
 % In each phase of the signal, determine the corresponding piece of time vector and envelope.
 
 % Attack: signal linearly increases from 0 to 1 in a seconds
-t = 0:1/fs:a-1/fs;
+t = 0:1/fs:a;
 e = interp1([0 a-1/fs], [0 1], t);
     
 % Decay: signal linearly decreases from 1 to s in d seconds
-tdelay = a:1/fs:(a+d-1/fs); 
+tdelay = (a+1/fs):1/fs:(a+d); 
 t = [t, tdelay];
-e = [e, interp1([a a+d-1/fs],[1 s], tdelay )];
+e = [e, interp1([a a+d],[1 s], tdelay )];
 
 % Sustain: signal stays at s for dur seconds
-tsustain = (a+d):1/fs:(a+d+dur-1/fs);
+tsustain = (a+d+1/fs):1/fs:(a+d+dur);
 esustain = ones(size(tsustain));
 esustain = esustain .* s;
 t = [t, tsustain];
 e = [e, esustain];
 
 % Release: signal linearly decreases from s to 0 in r seconds
-trelease = (a+d+dur):1/fs:(a+d+dur+r-1/fs);
+trelease = (a+d+dur+1/fs):1/fs:(a+d+dur+r);
 t = [t, trelease];
-e = [e, interp1([0 r-1/fs], [s 0], trelease)];
+e = [e, interp1([0 r], [s 0], trelease)];
